9f809523cd824744a72eaf4694561ac5d0a44dab
matheuspaiva1 <matheuspaiva0007@gmail.com>
2024-07-21 11:14:55 -0300

    feat: conexão do banco de dados

---
+++ api/src/main/resources/db/migration/V1__create-event-table.sql
@@ -0,0 +1,10 @@
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+CREATE TABLE event2 (
+    id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    description VARCHAR(250) NOT NULL,
+    img_url VARCHAR(100) NOT NULL,
+    event_url VARCHAR(100) NOT NULL,
+    date TIMESTAMP NOT NULL,
+    remote BOOLEAN NOT NULL
+);
